59aa1ba4dd35c9b3f1650389d83a8e6b3e3213a4
Josua Pangihutan Sitanggang <josuastg93@gmail.com>
2025-10-03 17:40:43 +0700

    enhance navbar for support category

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,25 +3,47 @@
     'fixed top-0 left-0 right-0 bg-white z-50 transition-shadow',
     { 'shadow-md': hasShadow }
   ]">
-    <nav class=" max-w-7xl mx-auto flex items-center justify-between px-6 py-4">
+    <nav class="max-w-7xl mx-auto flex items-center p-4 gap-x-8">
       <!-- Logo -->
-      <div class="text-xl text-gray-600 font-bold text-blackcursor-pointer">
-        Mini Store
+      <div class="text-xl cursor-pointer text-black font-bold">
+        <NuxtLink :to="{ path: '/' }">
+          Mini Store
+        </NuxtLink>
       </div>
 
       <!-- Menu -->
-      <ul class="flex space-x-6 text-gray-700 font-medium">
+      <ul class="flex space-x-6 text-gray-700 items-center font-medium">
         <li>
-          <NuxtLink to="/men" class="hover:text-black">Men</NuxtLink>
+          <NuxtLink :to="{ path: '/', query: { category: 'men\'s clothing' } }" :class="[
+            'hover:text-black',
+            route.query.category === `men's clothing` ? 'font-bold text-black' : ''
+          ]">
+            Men
+          </NuxtLink>
         </li>
         <li>
-          <NuxtLink to="/women" class="hover:text-black">Women</NuxtLink>
+          <NuxtLink :to="{ path: '/', query: { category: 'women\'s clothing' } }" :class="[
+            'hover:text-black',
+            route.query.category === `women's clothing` ? 'font-bold text-black' : ''
+          ]">
+            Women
+          </NuxtLink>
         </li>
         <li>
-          <NuxtLink to="/jewelry" class="hover:text-black">Jewelry</NuxtLink>
+          <NuxtLink :to="{ path: '/', query: { category: 'jewelery' } }" :class="[
+            'hover:text-black',
+            route.query.category === 'jewelery' ? 'font-bold text-black' : ''
+          ]">
+            Jewelry
+          </NuxtLink>
         </li>
         <li>
-          <NuxtLink to="/electronics" class="hover:text-black">Electronic</NuxtLink>
+          <NuxtLink :to="{ path: '/', query: { category: 'electronics' } }" :class="[
+            'hover:text-black',
+            route.query.category === 'electronics' ? 'font-bold text-black' : ''
+          ]">
+            Electronic
+          </NuxtLink>
         </li>
       </ul>
     </nav>
@@ -31,7 +53,7 @@
 <script setup lang="ts">
 
 const hasShadow = ref(false)
-
+const route = useRoute();
 const handleScroll = () => {
   hasShadow.value = window.scrollY > 10
 }
