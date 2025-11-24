985a41d61dd9abcde90b48b75ec6b8f50c6a0fa1
Josua Pangihutan Sitanggang <josuastg93@gmail.com>
2025-11-24 22:10:32 +0700

    handle error in vercel

--- server/api/products/index.ts
+++ server/api/products/index.ts
@@ -1,37 +1,45 @@
+export const config = {
+  runtime: 'nodejs'
+}
+
 export default defineEventHandler(async (event) => {
-  const query = getQuery(event)
-  const page = Number(query.page) || 1
-  const perPage = Number(query.perPage) || 8
-  const search = (query.search as string)?.toLowerCase() || ''
-  const category = (query.category as string)?.toLowerCase() || ''
+  try {
+    const query = getQuery(event)
+    const page = Number(query.page) || 1
+    const perPage = Number(query.perPage) || 8
+    const search = (query.search as string)?.toLowerCase() || ''
+    const category = (query.category as string)?.toLowerCase() || ''
 
-  // Ambil semua produk dari FakeStore
-  const products = await $fetch<any[]>('https://fakestoreapi.com/products')
+    const products = await $fetch<any[]>('https://fakestoreapi.com/products')
 
-  // Filter berdasarkan search (nama produk atau kategori)
-  let filtered = products
+    let filtered = products
 
-  if (category) {
-    filtered = products.filter((p) => p.category.toLowerCase() === category.toLowerCase())
-  }
+    if (category) {
+      filtered = filtered.filter((p) => p.category.toLowerCase() === category)
+    }
 
-  if (search) {
-    filtered = products.filter(
-      (p) =>
-        p.title.toLowerCase().includes(search) ||
-        p.category.toLowerCase().includes(search)
-    )
-  }
+    if (search) {
+      filtered = filtered.filter(
+        (p) =>
+          p.title.toLowerCase().includes(search) ||
+          p.category.toLowerCase().includes(search)
+      )
+    }
 
-  // Pagination logic
-  const total = filtered.length
-  const start = (page - 1) * perPage
-  const paginated = filtered.slice(start, start + perPage)
+    const total = filtered.length
+    const start = (page - 1) * perPage
+    const paginated = filtered.slice(start, start + perPage)
 
-  return {
-    data: paginated,
-    total,
-    page,
-    perPage,
+    return {
+      data: paginated,
+      total,
+      page,
+      perPage,
+    }
+  } catch (error) {
+    return sendError(event, createError({
+      statusCode: 500,
+      statusMessage: "Failed to fetch FakeStore API"
+    }))
   }
 })
